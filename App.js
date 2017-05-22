6e9f06db3a0ccce1dc01155e832a112888d2fa36
Łukasz <osmolski.lukasz@gmail.com>
2017-05-22 20:58:28 +0200

    1st

---
+++ App.js
@@ -0,0 +1,41 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': '1777',
+  'X-Auth-Token': 'd1f239676fc25c94ea8cc51881c24c4b'
+};
+
+$.ajaxSetup({
+	headers: myHeaders
+});
+
+
+$.ajax({
+    url: baseUrl + '/board',
+    method: 'GET',
+    success: function(response) {
+      setupColumns(response.columns);
+    }
+});
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+    });
+}
+
+function setupColumns(columns) {
+    columns.forEach(function (column) {
+  		var col = new Column(column.id, column.name);
+        board.createColumn(col);
+        setupCards(col, column.cards);
+    });
+}
+
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    	col.createCard(card);
+  	});
+}
